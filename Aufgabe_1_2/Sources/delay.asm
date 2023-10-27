4a2e0f374cece9eb9542521f006cb5970feb2ef9
blcksheep990 <timeit@hs-esslingen.de>
2023-10-27 18:36:12 +0200

    added commmentary on every line

--- Aufgabe_1_2/Sources/delay.asm
+++ Aufgabe_1_2/Sources/delay.asm
@@ -9,16 +9,16 @@
 .init: SECTION
 
 delay_0_5sec:
-        LDX #$000D ; Set the number of iterations for the delay
-        JSR delay_loop
-        RTS
+        LDX #$000D                      ; Set the number of iterations for the delay
+        JSR delay_loop                  ; Jump to subroutine delay_loop
+        RTS                             ; Return from Subroutine
 delay_loop:
-        DEX
-        LDY #$FFFF
-        CPX #0
-        BNE delay_loop_2
-        RTS
-delay_loop_2:
-        DEY
-        BNE delay_loop_2
-        BRA delay_loop
+        DEX                             ; Decrement the value of register X (iteration)
+        LDY #$FFFF                      ; Load the number of iteration in the Y register
+        CPX #0                          ; Compare the value of X with zero
+        BNE delay_loop_2                ; If the value in X is greater than zero jump to subroutine delay_loop_2
+        RTS                             ; Return from Subroutine
+delay_loop_2:                           ; Second loop because bit value is to high for 1 Byte to get the 0,5sec delay
+        DEY                             ; Decrement the value of register Y (iteration)
+        BNE delay_loop_2                ; If the value in register Y is not equal to zero go back to the start of the subroutine 
+        BRA delay_loop                  ; If the value is zero, the operation before is not true so the program is executing this operation instead and branch to subroutine delay_loop
--- Aufgabe_1_2/Sources/main.asm
+++ Aufgabe_1_2/Sources/main.asm
@@ -29,21 +29,21 @@ Entry:
         JSR initLED                     ; Execute Subroutine initLED
 
 reset:
-        CLRA
-        CLRB
-        MOVW #0, var1
-        BRA loop
+        CLRA                            ; Clear bits in register A
+        CLRB                            ; Clear bits in register B
+        MOVW #0, var1                   ; set bits in var1 to zero
+        BRA loop                        ; Branch to Subroutine loop
 
 loop:
-        LDD var1
-        ADDD #2
-        CPD #63
-        BGT reset
-        STD var1
-        JSR setLED
-        JSR delay_0_5sec
-        JSR toggleLED
-        BRA loop
+        LDD var1                        ; Load var1 into register D
+        ADDD #2                         ; Increment var1 which is loaded in register D with 2
+        CPD #63                         ; Compare value of register D with the value of 63 (0-63=64)
+        BGT reset                       ; If the value in D is greater than 63 branch back to subroutine reset
+        STD var1                        ; Store the incremented value of register D back to var1
+        JSR setLED                      ; Jump to subroutine setLED
+        JSR delay_0_5sec                ; Jump to subroutine delay_0_5sec
+        JSR toggleLED                   ; Jump to subroutine toggleLED
+        BRA loop                        ; Branch back to subroutine loop
 
 ;lightOn:
 ;LDD var1
