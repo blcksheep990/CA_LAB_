754a67153a5d1aa1770b14f2bfdb4893125bf173
blcksheep990 <timeit@hs-esslingen.de>
2023-10-27 18:14:38 +0200

    added Aufgabe_2_1

---
+++ Aufgabe_2_1/Sources/main.asm
@@ -0,0 +1,53 @@
+; Labor 1 - Aufgabe 1.2
+; Incrementing a value once per second and binary output to LEDs
+
+; export symbols
+        XDEF Entry, main
+
+; import symbols
+        XREF __SEG_END_SSTACK ; End of stack
+        XREF initLED, setLED, getLED, delay_0_5sec, toggleLED
+
+; include derivative specific macros
+        INCLUDE 'mc9s12dp256.inc'
+
+; RAM: Variable data section
+.data: SECTION
+var1: DS.W 1 ; one 16 bit variable
+
+; ROM: Constant data
+.const: SECTION
+
+; ROM: Code section
+.init: SECTION
+
+main: ; Begin of the program
+Entry:
+        LDS #__SEG_END_SSTACK           ; Initialize stack pointer
+        CLI                             ; Enable interrupts, needed for debugger
+
+        JSR initLED                     ; Execute Subroutine initLED
+
+reset:
+        CLRA
+        CLRB
+        MOVW #0, var1
+        BRA loop
+
+loop:
+        LDD var1
+        ADDD #2
+        CPD #63
+        BGT reset
+        STD var1
+        JSR setLED
+        JSR delay_0_5sec
+        JSR toggleLED
+        BRA loop
+
+;lightOn:
+;LDD var1
+;EORB #$FF
+;       STAB PORTB
+;        RTS
+
