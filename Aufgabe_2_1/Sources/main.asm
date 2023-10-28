6da39a9b5daf28154ca7effb50008408c2cbaf22
flo100500 <florian@team-fink.de>
2023-10-28 15:39:28 +0200

    Added Template lab1c instead of copy of lab1a

--- Aufgabe_2_1/Sources/main.asm
+++ Aufgabe_2_1/Sources/main.asm
@@ -1,48 +1,47 @@
-; Labor 1 - Aufgabe 1.2
-; Incrementing a value once per second and binary output to LEDs
-
-;# done by Tim:
+;   Labor 1 - Vorbereitungsaufgabe 2.4
+;   Convert a zero-terminated ASCIIZ string to lower characters
+;   Main program
+;
+;   Computerarchitektur
+;   (C) 2019-2022 J. Friedrich, W. Zimmermann, R. Keller
+;   Hochschule Esslingen
+;
+;   Author:   R. Keller, Jul 4, 2019
+;            (based on code provided by J. Friedrich, W. Zimmermann)
+;   Modified: -
+;
 
 ; export symbols
         XDEF Entry, main
 
 ; import symbols
         XREF __SEG_END_SSTACK           ; End of stack
-        XREF initLED, setLED, getLED, delay_0_5sec, toggleLED
+        XREF toLower                    ; Referenced from other object file
 
 ; include derivative specific macros
         INCLUDE 'mc9s12dp256.inc'
 
+; Defines
+
 ; RAM: Variable data section
-.data: SECTION
-var1: DS.W 1                            ; one 16 bit variable
+.data:  SECTION
+Vtext:  DS.B    80                      ; Please store String here
 
 ; ROM: Constant data
 .const: SECTION
+Ctext:  DC.B  "Test 12345 *!? ABCDE abcde zZ", 0
 
 ; ROM: Code section
-.init: SECTION
+.init:  SECTION
 
 main:                                   ; Begin of the program
 Entry:
-        LDS #__SEG_END_SSTACK           ; Initialize stack pointer
+        LDS  #__SEG_END_SSTACK          ; Initialize stack pointer
         CLI                             ; Enable interrupts, needed for debugger
 
-        JSR initLED                     ; Execute Subroutine initLED
-
-reset:
-        CLRA                            ; Clear bits in register A
-        CLRB                            ; Clear bits in register B
-        MOVW #0, var1                   ; set bits in var1 to zero
-        BRA loop                        ; Branch to Subroutine loop
-
-loop:
-        LDD var1                        ; Load var1 into register D
-        ADDD #2                         ; Increment var1 which is loaded in register D with 2
-        CPD #63                         ; Compare value of register D with the value of 63 (0-63=64)
-        BGT reset                       ; If the value in D is greater than 63 branch back to subroutine reset
-        STD var1                        ; Store the incremented value of register D back to var1
-        JSR setLED                      ; Jump to subroutine setLED
-        JSR delay_0_5sec                ; Jump to subroutine delay_0_5sec
-        JSR toggleLED                   ; Jump to subroutine toggleLED
-        BRA loop                        ; Branch back to subroutine loop
+;       ... ??? ...                     ; Add your code here
+;
+;  To Do
+;       Copy string from Ctext in ROM to Vtext in RAM
+;       Load pointer to string in D
+;       Call subroutine toLower
