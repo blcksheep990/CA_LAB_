e07d85e57c76cccae6c04b85ef717c092e1b1953
flo100500 <74304150+flo100500@users.noreply.github.com>
2023-11-20 15:39:07 +0100

    Update clock.c

--- Aufgabe_2_3_2/Sources/clock.c
+++ Aufgabe_2_3_2/Sources/clock.c
@@ -1,6 +1,8 @@
 #include <hidef.h>                              // Common defines
 #include "wrappers.h"
 
+#define SELECT12HOURS
+
 // Global variables
 struct time_t{
     char hour;
@@ -10,6 +12,7 @@ struct time_t{
 
 enum clockMode_t {NORMAL, SET};
 enum clockMode_t clockMode = NORMAL;
+enum am_or_pm_t {AM, PM} am_or_pm;
 
 // clock help functions
 
@@ -26,14 +29,41 @@ void timeToString(){
     decToASCII_Wrapper(temp, time.second);
     outputString[6] = temp[4];
     outputString[7] = temp[5];
+    #ifdef SELECT12HOURS
+	if(am_or_pm == AM){
+	    outputString[8] = 'a';
+	} else {
+	    outputString[8] = 'p';
+	}
+	outputString[9] = 'm';
+    #endif
+}
+
+void toggle_AM_PM(){
+    if(am_or_pm == AM){
+	am_or_pm = PM;
+    } else {
+	am_or_pm = AM;
+    }
 }
 
 void increaseHour(){
+    #ifdef SELECT12HOURS
+    if(time.hour == 11){
+        toggle_AM_PM();
+    }
+    if(time.hour == 12){
+	time.hour = 1;
+    } else {
+        time.hour++;
+    }
+    #else
     if(time.hour == 23){
         time.hour = 0;
     } else {
         time.hour++;
     }
+    #endif
 }
 
 void increaseMinute(){
@@ -69,18 +99,19 @@ void initClock(){
     time.hour = 11;
     time.minute = 59;
     time.second = 30;
+    am_or_pm = PM;
     initLED();
 }
 
 void tickClock(){
-    if(PTH & 0x04) toggleClockMode();
+    if(~PTH & 0x01) toggleClockMode();
     if (clockMode == NORMAL){
         increaseSecond();
         toggleLED_Wrapper(0x1);
     } else {
-        if(PTH & 0x08) increaseHour();
-        if(PTH & 0x10) increaseMinute();
-        if(PTH & 0x20) increaseSecond();
+        if(~PTH & 0x02) increaseHour();
+        if(~PTH & 0x04) increaseMinute();
+        if(~PTH & 0x08) increaseSecond();
     }
 
     timeToString();
