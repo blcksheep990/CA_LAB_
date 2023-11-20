b3d63c7463df00fd397a8cd9bcbf55e720d96860
flo100500 <74304150+flo100500@users.noreply.github.com>
2023-11-20 15:40:58 +0100

    Update thermometer.c

--- Aufgabe_2_3_2/Sources/thermometer.c
+++ Aufgabe_2_3_2/Sources/thermometer.c
@@ -2,18 +2,20 @@
 #include "wrappers.h"
 
 void initThermo(){
-    ATD0CTL2 |= 0xC0;
-    ATD0CTL3 |= 0x80;
-    ATD0CTL4 |= 0x05;
-    ATD0CTL5 |= 0x87;
+    ATD0CTL2 = 0xC0;
+    ATD0CTL3 = 0x80;
+    ATD0CTL4 = 0x05;
 }
 
 void updateThermo(){
     char temperature;
     char temp[9];
+
+    ATD0CTL5 = 0x87;
+
     while (ATD0STAT0 & 0x80 == 0x00){}
-    ATD0DR0 = 1024;
-    temperature = ATD0DR0 * 50 / 512 - 30;
+    //ATD0DR0 = 1024;
+    temperature = ATD0DR0 * 50 / 511 - 30;
     decToASCII_Wrapper(temp, temperature);
     if (temperature < 0){
         outputString[11] = '-';
@@ -25,7 +27,7 @@ void updateThermo(){
         outputString[12] = ' ';
     }
     outputString[13] = temp[5];
-    outputString[14] = 176;
+    outputString[14] = 'o';
     outputString[15] = 'C';
     outputString[16] = 0;
 }
